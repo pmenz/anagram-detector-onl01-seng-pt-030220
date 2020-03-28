27902dbd515c31bc79e441290cccb490800c3958
Pablo Menz <p.menz.r@gmail.com>
2020-03-28 01:03:57 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @words = word
+  end
+  
+  def match(words)
+    
+    words.map{ |i| "#{i}" }.chars.sort
+    #array.map{ |i|  %Q('#{i}') }.join(',')
+  end
+end
