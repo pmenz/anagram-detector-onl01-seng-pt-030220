b61329e59fa0fbcf036e42936b562a6f600fb0d3
Pablo Menz <p.menz.r@gmail.com>
2020-03-28 01:24:02 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(words)
+  
+  words.select {|x| x.split("").sort == @word.split("").sort}
+     
+   # a= words.map{ |i| "#{i}" }.sort
+   # a.each do |i|
+   # a[i].split(//).sort == @word.split(//).sort
+      
+      
+        #
+     # end
+    
+    #array.map{ |i|  %Q('#{i}')  }.join(',')
+  end
+end
