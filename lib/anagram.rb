74aab5252aba8cccfedcb016e13ac8fcef220c35
Pablo Menz <p.menz.r@gmail.com>
2020-03-28 01:21:57 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,17 +4,19 @@ class Anagram
   attr_accessor :word
   
   def initialize(word)
-    @words = word
+    @word = word
   end
   
   def match(words)
-    
+  
+  #array.select {|x| x.split("").sort == @name.split("").sort}
+     
     a= words.map{ |i| "#{i}" }.sort
     a.each do |i|
        a[i].split(//).sort == word.split(//).sort
         a[i]
       
-        
+        #
       end
     
     #array.map{ |i|  %Q('#{i}')  }.join(',')
